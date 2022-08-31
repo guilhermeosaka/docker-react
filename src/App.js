b8904c29258ea3b0d1466f2655594a3c0a53143b
Guilherme Osaka <guilhermeosaka@gmail.com>
2022-08-30 21:40:21 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Got it? Yeah, I did. Amazing. Testing again, now with docker compose.
         </p>
         <a
           className="App-link"
